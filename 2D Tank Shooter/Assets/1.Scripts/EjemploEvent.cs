290ab72e6d86c8eed0ce7afad083cabe206a503f
Hydekyle <hydekyle@hotmail.com>
2018-02-02 09:27:26 +0200

    Add files via upload
    Ejemplo de Event para futuras referencias.

---
+++ 2D Tank Shooter/Assets/1.Scripts/EjemploEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class EjemploEvent : MonoBehaviour {
+
+    public static EjemploEvent Instance;
+    public delegate void Print(string s);
+    public static event Print onSpaceKeyPressed;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onSpaceKeyPressed != null)
+        {
+            onSpaceKeyPressed("Vaya, vaya");
+        }
+    }
+}
